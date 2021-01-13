8bd8cd2c553b247d2fe63d4df6d00af3283c231a
Yasinthon <yasinthon_r@elearning.cmu.ac.th>
2021-01-13 13:57:34 +0700

    LAB 8-1 inheritance 1

---
+++ Week(fix)/Chef.java
@@ -0,0 +1,15 @@
+package Week6;
+
+public class Chef extends Person{
+    String food;
+    public Chef(String name, int age, String gender){
+        this.age =age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void chef(){
+        System.out.println(name + " cooking a " + food );
+        System.out.println();
+    }
+}
---
+++ Week(fix)/Person.java
@@ -0,0 +1,21 @@
+package Week6;
+
+public class Person{
+    String name;
+    int age;
+    String gender;
+    public Person(){
+
+    }
+    public Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void introduce(){
+        System.out.println("My name is " + name);
+        System.out.println(name + " is " + age + " years old");
+        System.out.println(name + " is " + gender);
+    }
+}
